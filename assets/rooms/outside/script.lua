b13fa95469dc3de75a39f9f73644efdf47c80d38
Sandy Maguire <sandy.g.maguire@gmail.com>
2013-01-17 11:37:52 -0500

    Fixed a script bug in outside.

--- assets/rooms/outside/script.lua
+++ assets/rooms/outside/script.lua
@@ -1,4 +1,6 @@
-require "classes/polygon"
+import room from Adventure
+
+mrequire "classes/polygon"
 
 
     local poly = Polygon.new(550, 406, 571, 404, 584, 420, 584, 439, 574, 454, 566, 454, 570, 476, 573, 514, 571, 540, 567, 572, 567, 598, 553, 599, 553, 562, 556, 535, 554, 517, 550, 488, 551, 454, 547, 452, 536, 438, 534, 415)
